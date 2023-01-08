7eb297be3c23514d35dfe7fdab3a7266668b4c45
nicathuseynli <nijatkhh@code.edu.az>
2023-01-09 01:42:10 +0400

    front-final-project

--- assetss/js/home.js
+++ assetss/js/home.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+  $(".fa-bars").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".menu_respons_burger").toggleClass("nav-toggle");
+  });
+});
+$(document).ready(function () {
+  $(".dropdown-toggle").click(function () {
+    $(this).next(".dropdown").slideToggle();
+    $(this).next(".pages_drop").slideToggle();
+    $(this).next(".shop_drop").slideToggle();
+    $(this).next(".product_drop").slideToggle();
+    $(this).next(".shop_pages_drop").slideToggle();
+    $(this).next(".shop_types_drop").slideToggle();
+    $(this).next(".portfoilio_drop").slideToggle();
+    $(this).next(".shop_layouts_drop").slideToggle();
+    $(this).next(".blog_drop").slideToggle();
+    $(this).next(".elements_blog").slideToggle();
+    $(this).next(".categories_dropdown").slideToggle();
+  });
+  $(document).click(function (e) {
+    let target = e.target;
+    if (
+      !$(target).is(".dropdown-toggle") &&
+      !$(target).parents().is(".dropdown-toggle")
+    ) {
+      $(".dropdown").slideUp();
+      $(".pages_drop").slideUp();
+      $(".shop_drop").slideUp();
+      $(".product_drop").slideUp();
+      $(".shop_pages_drop").slideUp();
+      $(".shop_types_drop").slideUp();
+      $(".shop_layouts_drop").slideUp();
+      $(".portfoilio_drop").slideUp();
+      $(".blog_drop").slideUp();
+      $(".elements_drop").slideUp();
+      $(".categories_dropdown").slideUp();
+    }
+  });
+});
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
       center: true,
       loop: true,
@@ -31,5 +71,5 @@ $(document).ready(function () {
       },
     });
   });
- 
+
   
